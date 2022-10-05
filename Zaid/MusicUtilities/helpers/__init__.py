e72a7fdd139f9e67088fb9e0faf45ba5376ee5bd
SourseZeZo <115050402+SourseZeZo@users.noreply.github.com>
2022-10-05 17:21:10 +0200

    Delete Zaid directory

--- Zaid/MusicUtilities/helpers/__init__.py
+++
@@ -1,2 +0,0 @@
-from pyrogram import Client
-from Zaid.config import API_ID, API_HASH
--- Zaid/MusicUtilities/tgcallsrun/__init__.py
+++
@@ -1,9 +0,0 @@
-from os import listdir, mkdir
-from pyrogram import Client
-from ZeZo import config
-from ZeZo.MusicUtilities.tgcallsrun.queues import (clear, get, is_empty, put, task_done)
-from ZeZo.MusicUtilities.tgcallsrun.downloader import download
-from ZeZo.MusicUtilities.tgcallsrun.convert import convert
-from ZeZo.MusicUtilities.tgcallsrun.music import run
-from ZeZo.MusicUtilities.tgcallsrun.music import smexy as ASS_ACC
-smexy = 1
